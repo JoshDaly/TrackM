073c0a57e7b4710463723bbde5839ec23f48bc3f
minillinim <mike@mikeimelfort.com>
2014-07-14 15:01:05 +1000

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='TrackM',
-    version='0.1.0',
+    version='0.1.1',
     author='Josh Daly, Michael Imelfort',
     author_email='joshua.daly@uqconnect.edu.au',
     packages=['trackm'],
