42ebbd878e1b4476017fcabacb3cfd057eaa9c40
Josh <josh@josh-VirtualBox.(none)>
2014-07-23 14:48:25 +1000

    view testing

--- misc/view.py
+++ misc/view.py
@@ -439,10 +439,10 @@ class View(object):
                 #print hitList
                 stdDev = np.array(hitList)
                 standardised.append(np.std(stdDev, dtype=np.float64))
-                print standardised
+                #print standardised
             except KeyError:
                 standardised.append(0)
-
+            print standardised
         x,y = percList,normalisedHits
         #print x
         #print y
