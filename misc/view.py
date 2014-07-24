66994d6e91d1f17237a40d9105802fb36633991d
Josh <josh@josh-VirtualBox.(none)>
2014-07-24 12:25:16 +1000

    view testing

--- misc/view.py
+++ misc/view.py
@@ -492,9 +492,9 @@ class View(object):
             normaliseDirty = 0
             # create x and y coordinates for line graph
             try:
-                print self.DD.roundedComparisons[perc], self.HD.roundedDistance[perc], self.DD.dirtyRoundedHits[perc]
+                print perc, self.DD.roundedComparisons[perc], self.HD.roundedDistance[perc], self.DD.dirtyRoundedHits[perc]
             except KeyError:
-                print self.DD.roundedComparisons[perc]
+                print perc, self.DD.roundedComparisons[perc]
             
             c = self.DD.roundedComparisons[perc]/float(100)
             try:
